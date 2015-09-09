feb35d73268c7e7b71470b32f775b712a107db7e
Dominik Jungowski <Dominik@Jungowski.de>
2015-09-09 23:34:33 +0200

    custom sticky note text

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5,10 +5,13 @@ $(window).on('load', function() {
 	mainDocument.on('click', function(event) {
 		var x = event.pageX;
 		var y = event.pageY;
-		var newStickyNote = $('<div class="sticky-note">Foo</div>');
+		var newStickyNote = $('<div class="sticky-note"></div>');
+		var newStickyNoteTextArea = $('<textarea placeholder="empty sticky note"></textarea>');
+		newStickyNote.append(newStickyNoteTextArea);
 		newStickyNote.css('left', x);
 		newStickyNote.css('top', y);
 		$(document.body).append(newStickyNote);
+		newStickyNoteTextArea.focus();
 	});
 
 });
