e96e4909c3cde559065fa0c66bf786bcbd4ed0db
Dominik Jungowski <Dominik@Jungowski.de>
2015-09-09 23:41:15 +0200

    Don't create sticky if clicked inside sticky

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,13 @@ $(window).on('load', function() {
 	var mainDocument = $(document);
 
 	mainDocument.on('click', function(event) {
+		var clickTarget = $(event.target);
+		// Do nothing if the user clicked inside a sticky note
+		var clickedInsideStickyNote = clickTarget.hasClass('sticky-note') || clickTarget.parent('.sticky-note').size() > 0;
+		if (clickedInsideStickyNote) {
+			return;
+		}
+
 		var x = event.pageX;
 		var y = event.pageY;
 		var newStickyNote = $('<div class="sticky-note"></div>');
